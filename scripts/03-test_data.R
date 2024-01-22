0cdcb1e579bf5a7746c48c39e9a4f8d9baa612b1
Carly Penrose <carly.penrose@theijf.org>
2024-01-22 23:28:49 +0000

    Test data

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -15,6 +15,67 @@ cleaned_fire_data <-
 
 head(cleaned_fire_data)
 
+#running the tests
+cleaned_fire_data$ward |> min() == 1
+cleaned_fire_data$ward |> max() == 25
+cleaned_fire_data$year |> min() == 2018
+cleaned_fire_data$year |> max() == 2022
+cleaned_fire_data$civilian_casualties |> min() == 0
+cleaned_fire_data$civilian_casualties |> class() == "numeric"
+
 #counting how many fire incidents happened per year
 cleaned_fire_data |>
   count(year)
+
+#counting how many fire incidents happened in each ward 
+print(
+  cleaned_fire_data |>
+    count(ward),
+  n=25)
+
+# Calculate the sum of civilian casualties per year
+sum_per_year <- cleaned_fire_data |> group_by(year) |> 
+  summarize(total_casualties = sum(civilian_casualties))
+
+# Display the result
+print(sum_per_year)
+
+# Calculate the sum of civilian casualties in each ward
+print(
+  sum_per_ward <- cleaned_fire_data |> group_by(ward) |> 
+    summarize(total_casualties = sum(civilian_casualties)),
+  n=25)
+
+head(sum_per_ward)
+
+#create a graph of the number of fire incidents per ward 
+
+cleaned_fire_data |>
+  ggplot(aes(x = ward)) + # aes abbreviates "aesthetics" 
+  geom_bar()
+
+cleaned_fire_data |>
+  ggplot(aes(x = ward)) +
+  geom_bar() +
+  theme_minimal() + # Make the theme neater
+  labs(x = "Ward", y = "Number of fires") # Make labels more meaningful
+
+
+#create a graph of number of fire incidents per year 
+cleaned_fire_data |>
+  ggplot(aes(x = year)) +
+  geom_bar()
+
+cleaned_fire_data |>
+  ggplot(aes(x = year)) +
+  geom_bar() +
+  theme_minimal() + 
+  labs(x = "Year", y = "Number of fires") 
+
+#create a graph of the number of civilian deaths due to fires per ward 
+
+
+
+#create a graph of the number of civilian deaths due to fires per year
+
+
